d87ea886709fa1c17f7cabd2a35a57a203251ef0
Pocket <p0ck31@gmail.com>
2015-03-24 10:39:33 -0700

    Add routes! And title binding on post.html!

--- scripts/app.js
+++ scripts/app.js
@@ -3,17 +3,28 @@
 var app = angular
   .module('TaskNinjaApp', [
     'ngAnimate',
-    'ngResource',    
-    'ngRoute',    
+    'ngResource',
+    'ngRoute',
     'firebase'
   ])
-  .constant('FURL', 'https://your-firebase.firebaseio.com/')  
+  .constant('FURL', 'https://your-firebase.firebaseio.com/')
   .config(function ($routeProvider) {
-    $routeProvider      
+    $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html'        
+        templateUrl: 'views/main.html'
+      })
+      .when('/post', {
+        templateUrl: 'views/post.html',
+        controller: 'TaskController'
+      })
+      .when('/edit', {
+        templateUrl: 'views/edit.html'
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html'
       })
       .otherwise({
         redirectTo: '/'
       });
+
   });
